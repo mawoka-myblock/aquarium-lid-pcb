b762c52e45805b74989db89a947ddedc7c485cde
Mawoka <git@mawoka.eu>
2026-05-01 18:02:14 +0200

    Add mqtt

---
+++ firmware/src/tasks/mqtt/discovery.rs
@@ -0,0 +1,179 @@
+use embedded_mqttc::QoS;
+
+use crate::StaticMqttClient;
+
+const WATER_TEMP_CFG: &'static str = r#"
+{
+"name":"Water Temperature",
+"stat_t":"aquarium/sensor/water/state",
+"val_tpl":"{{value_json.temp}}",
+"uniq_id":"aq_w_temp",
+"unit_of_meas":"°C",
+"sug_dsp_prc":1,
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+const AIR_TEMP_CFG: &'static str = r#"
+{
+"name":"Air Temperature",
+"stat_t":"aquarium/sensor/air/state",
+"val_tpl":"{{value_json.temperature}}",
+"uniq_id":"aq_a_temp",
+"unit_of_meas":"°C",
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+const AIR_HUM_CFF: &'static str = r#"
+{
+"name":"Air Humidity",
+"stat_t":"aquarium/sensor/air/state",
+"val_tpl":"{{value_json.humidity}}",
+"uniq_id":"aq_a_hum",
+"unit_of_meas":"%",
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+const FAN_SWITCH: &'static str = r#"
+{
+"name":"Fan Switch",
+"stat_t":"aquarium/control/fan/state",
+"cmd_t":"aquarium/control/fan/set",
+"pl_on":"ON",
+"pl_off":"OFF",
+"uniq_id":"aq_fan",
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+const BUZZER_SWITCH: &'static str = r#"
+{
+"name":"Buzzer Switch",
+"stat_t":"aquarium/control/buzzer/state",
+"cmd_t":"aquarium/control/buzzer/set",
+"pl_on":"ON",
+"pl_off":"OFF",
+"uniq_id":"aq_buzzer",
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+const FAN_ON_ABOVE: &'static str = r#"
+{
+"name":"Fan On Threshold",
+"stat_t":"aquarium/sensor/config/state",
+"cmd_t":"aquarium/control/config/fan_above/set",
+"val_tpl": "{{value_json.fan_on_threshold}}",
+"uniq_id":"aq_fan_above",
+"unit_of_meas":"°C",
+"mode":"box",
+"step":0.1,
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+const FAN_OFF_BELOW: &'static str = r#"
+{
+"name":"Fan Off Threshold",
+"stat_t":"aquarium/sensor/config/state",
+"cmd_t":"aquarium/control/config/fan_below/set",
+"val_tpl": "{{value_json.fan_off_threshold}}",
+"uniq_id":"aq_fan_below",
+"unit_of_meas":"°C",
+"mode":"box",
+"step":0.1,
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+const LED_BRIGHTNESS: &'static str = r#"
+{
+"name":"LED Brightness",
+"stat_t":"aquarium/sensor/config/state",
+"cmd_t":"aquarium/control/config/led_brightness/set",
+"val_tpl": "{{value_json.led_brightness}}",
+"uniq_id":"aq_led_brightness",
+"mode":"box",
+"min":0,
+"max":255,
+"dev":{"ids":"aq", "name":"Aquarium Fan"}
+}
+"#;
+
+pub async fn publish_discovery(client: StaticMqttClient) {
+    client
+        .publish(
+            "homeassistant/sensor/aquarium_water/config",
+            WATER_TEMP_CFG.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+    client
+        .publish(
+            "homeassistant/sensor/aquarium_air_temp/config",
+            AIR_TEMP_CFG.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+    client
+        .publish(
+            "homeassistant/sensor/aquarium_air_hum/config",
+            AIR_HUM_CFF.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+    client
+        .publish(
+            "homeassistant/switch/aquarium_fan/config",
+            FAN_SWITCH.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+    client
+        .publish(
+            "homeassistant/switch/aquarium_buzzer/config",
+            BUZZER_SWITCH.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+
+    client
+        .publish(
+            "homeassistant/number/aquarium_fan_on_thres/config",
+            FAN_ON_ABOVE.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+    client
+        .publish(
+            "homeassistant/number/aquarium_fan_off_thres/config",
+            FAN_OFF_BELOW.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+    client
+        .publish(
+            "homeassistant/number/aquarium_led_brightness/config",
+            LED_BRIGHTNESS.as_bytes(),
+            QoS::AtLeastOnce,
+            true,
+        )
+        .await
+        .unwrap();
+}
---
+++ firmware/src/tasks/mqtt/mod.rs
@@ -0,0 +1,163 @@
+use alloc::borrow::ToOwned;
+use defmt::{info, unwrap};
+mod discovery;
+
+pub use discovery::publish_discovery;
+
+use crate::{
+    AirData, COMMANDS, DATACHANNEL, StaticMqttClient, StaticMqttState, WaterTemperatureResponse,
+};
+use embedded_mqttc::QoS;
+
+#[embassy_executor::task]
+pub async fn run_mqtt_loop(mqtt_state: &'static StaticMqttState) {
+    unwrap!(mqtt_state.run().await);
+}
+
+const BASE_TOPIC: &'static str = "aquarium";
+
+#[embassy_executor::task]
+pub async fn listen_datachannel(client: StaticMqttClient) {
+    let mut sub = DATACHANNEL.subscriber().unwrap();
+    let mut buf = [0u8; 512];
+    loop {
+        let msg = sub.next_message_pure().await;
+        let (topic, payload, retain): (&str, &[u8], bool) = match msg {
+            crate::DataMessage::AirData(d) => {
+                if let Some(data) = d {
+                    let d_len = unwrap!(serde_json_core::to_slice::<AirData>(&data, &mut buf));
+                    (
+                        &(BASE_TOPIC.to_owned() + "/sensor/air/state"),
+                        &buf[..d_len],
+                        false,
+                    )
+                } else {
+                    continue;
+                }
+            }
+            crate::DataMessage::WaterTemperature(d) => {
+                let d_len = unwrap!(serde_json_core::to_slice::<WaterTemperatureResponse>(
+                    &WaterTemperatureResponse { temp: Some(d) },
+                    &mut buf
+                ));
+                (
+                    &(BASE_TOPIC.to_owned() + "/sensor/water/state"),
+                    &buf[..d_len],
+                    false,
+                )
+            }
+        };
+
+        info!("publishing: {}", topic);
+        match client
+            .publish(topic, payload, QoS::AtMostOnce, retain)
+            .await
+        {
+            Ok(_) => (),
+            Err(e) => defmt::error!("{:?}", e),
+        };
+    }
+}
+
+#[embassy_executor::task]
+pub async fn listen_commandchannel(client: StaticMqttClient) {
+    let mut sub = COMMANDS.subscriber().unwrap();
+    let mut buf = [0u8; 512];
+    loop {
+        let msg = sub.next_message_pure().await;
+        let (topic, payload, retain): (&str, &[u8], bool) = match msg {
+            crate::Command::FanOn => (
+                &(BASE_TOPIC.to_owned() + "/control/fan/state"),
+                "ON".as_bytes(),
+                true,
+            ),
+            crate::Command::FanOff => (
+                &(BASE_TOPIC.to_owned() + "/control/fan/state"),
+                "OFF".as_bytes(),
+                true,
+            ),
+            crate::Command::BuzzerOn => (
+                &(BASE_TOPIC.to_owned() + "/control/buzzer/state"),
+                "ON".as_bytes(),
+                true,
+            ),
+            crate::Command::BuzzerOff => (
+                &(BASE_TOPIC.to_owned() + "/control/buzzer/state"),
+                "OFF".as_bytes(),
+                true,
+            ),
+            crate::Command::Reconfigure(d) => {
+                let d_len = unwrap!(serde_json_core::to_slice::<crate::Config>(&d, &mut buf));
+                (
+                    &(BASE_TOPIC.to_owned() + "/sensor/config/state"),
+                    &buf[..d_len],
+                    true,
+                )
+            }
+            _ => continue,
+        };
+
+        info!("publishing: {}", topic);
+        match client
+            .publish(topic, payload, QoS::AtLeastOnce, retain)
+            .await
+        {
+            Ok(_) => (),
+            Err(e) => defmt::error!("{:?}", e),
+        };
+    }
+}
+
+#[embassy_executor::task]
+pub async fn listen_mqtt(client: StaticMqttClient) {
+    client
+        .subscribe(
+            &(BASE_TOPIC.to_owned() + "/control/+/set"),
+            QoS::AtLeastOnce,
+        )
+        .await
+        .unwrap();
+
+    let mut sub = client.subscribe_received_publishes().unwrap();
+    let cmd_pub = COMMANDS.immediate_publisher();
+    loop {
+        let msg = sub.next_message_pure().await;
+        let pl = str::from_utf8(msg.payload.as_slice()).unwrap();
+        let parts: heapless::Vec<&str, 8> = msg.topic_name.split('/').collect();
+        info!("Msg recvd, parts: {}", parts);
+        match parts.as_slice() {
+            [BASE_TOPIC, "control", device, "set"] => match device {
+                &"fan" => match pl {
+                    "ON" => cmd_pub.publish_immediate(crate::Command::FanOn),
+                    "OFF" => cmd_pub.publish_immediate(crate::Command::FanOff),
+                    _ => (),
+                },
+                &"buzzer" => match pl {
+                    "ON" => cmd_pub.publish_immediate(crate::Command::BuzzerOn),
+                    "OFF" => cmd_pub.publish_immediate(crate::Command::BuzzerOff),
+                    _ => (),
+                },
+                _ => (),
+            },
+            [BASE_TOPIC, "control", "config", action, "set"] => match action {
+                &"fan_above" => {
+                    let mut cfg = crate::Config::get_from_signal();
+                    cfg.fan_on_threshold = pl.parse::<f32>().unwrap();
+                    cmd_pub.publish_immediate(crate::Command::Reconfigure(cfg));
+                }
+                &"fan_below" => {
+                    let mut cfg = crate::Config::get_from_signal();
+                    cfg.fan_off_threshold = pl.parse::<f32>().unwrap();
+                    cmd_pub.publish_immediate(crate::Command::Reconfigure(cfg));
+                }
+                &"led_brightness" => {
+                    let mut cfg = crate::Config::get_from_signal();
+                    cfg.led_brightness = pl.parse::<u8>().unwrap();
+                    cmd_pub.publish_immediate(crate::Command::Reconfigure(cfg));
+                }
+                _ => (),
+            },
+            _ => (),
+        }
+    }
+}
